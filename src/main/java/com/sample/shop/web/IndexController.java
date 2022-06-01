76beecdc9e4c440973915851d9f8c776e26b43e6
siwonscool <kimsea13@naver.com>
2022-06-02 03:17:24 +0900

    프론트 만지작

--- src/main/java/com/sample/shop/web/IndexController.java
+++ src/main/java/com/sample/shop/web/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/member/join")
+    public String join(){
+        return "member/member-register";
+    }
 }
